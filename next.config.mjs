da4bf8605ef25a72515aade41e48288ef2bd5b22
somacruzlistic <rotuber999@gmail.com>
2025-05-17 16:00:04 +0530

    Add multiple build exclusion configurations

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,17 @@ const nextConfig = {
   images: {
     domains: ['image.tmdb.org', 'i.ytimg.com'],
   },
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Don't include API routes in client-side builds
+      config.module.rules.push({
+        test: /app\/api\/.*/,
+        loader: 'ignore-loader',
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
